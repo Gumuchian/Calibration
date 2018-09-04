6e160703b1c67a04066770e1f4f9e56a362f7184
Gumuchian <paulgumuchian@yahoo.fr>
2018-09-04 17:25:36 +0200

    Pattern points + threshold as main parameters

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     //MainWindow w;
-    Processing proc(2048,40);
     QStringList args = QCoreApplication::arguments();
+    Processing proc(args[3].toInt(),args[4].toInt());
     proc.calibrate(args[1],args[2]);
     //w.show();
     a.quit();
