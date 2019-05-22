07c02601001ab7ee1490affd89803976572ace9a
Gumuchian <paulgumuchian@yahoo.fr>
2019-05-22 19:06:21 +0200

    Update

--- processing.h
+++ processing.h
@@ -1,6 +1,8 @@
 #ifndef PROCESSING_H
 #define PROCESSING_H
 #include "event_processor.h"
+#include <QFile>
+#include <QTextStream>
 
 using namespace boost::numeric::ublas;
 
@@ -10,6 +12,7 @@ public:
     Processing();
     Processing(int Nbre, double threshold);
     void calibrate(QString pulse_path, QString noise_path);
+    //void openfile(QFile file, long position);
 private:
     int N;
     Event_Processor EP;
